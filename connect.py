be586b9fa751eae2f0530d84c9ee91a3e7e8facb
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-04-03 06:57:01 +0300

    write the main function

--- connect.py
+++ connect.py
@@ -2,3 +2,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import pyautogui as pag
+
+
+
+
+def main():
+    # url of LinkedIn
+    url = “http://linkedin.com/"  
+    # url of LinkedIn network page
+    network_url = “http://linkedin.com / mynetwork/" 
+    # path to browser web driver
+    driver = webdriver.Chrome('C:\\Program Files\\Web Driver\\chromedriver.exe'')
+    driver.get(url)
+
+# Driver's code
+if __name__ == __main__:
+    main()
