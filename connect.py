baf8e87175197d1cb9305a5cb54939140a868ed4
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-04-03 07:00:41 +0300

    authentication page and then we need to login

--- connect.py
+++ connect.py
@@ -4,15 +4,28 @@ from selenium.webdriver.common.keys import Keys
 import pyautogui as pag
 
 
-
-
+def login():
+    # Getting the login element
+    username = driver.find_element_by_id(“login-email”)  
+    # Sending the keys for username     
+    username.send_keys(“username”)       
+    # Getting the password element                            
+    password = driver.find_element_by_id(“login-password”) 
+    # Sending the keys for password   
+    password.send_keys(“password”)               
+    # Getting the tag for submit button           
+    driver.find_element_by_id(“login-submit”).click() 
+                              
+def goto_network():
+    driver.find_element_by_id(“mynetwork-tab-icon”).click()
+                              
 def main():
     # url of LinkedIn
     url = “http://linkedin.com/"  
     # url of LinkedIn network page
     network_url = “http://linkedin.com / mynetwork/" 
     # path to browser web driver
-    driver = webdriver.Chrome('C:\\Program Files\\Web Driver\\chromedriver.exe'')
+    driver = webdriver.Chrome('C:\\Program Files\\Web Driver\\chromedriver.exe')
     driver.get(url)
 
 # Driver's code
