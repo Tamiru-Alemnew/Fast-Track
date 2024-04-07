caaf51684b739ec4a19fc0296fd0855c13bee7d0
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-04-07 21:08:55 +0300

    finilize the Project

--- internate speed/internet_speed/lib/main.dart
+++ internate speed/internet_speed/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:speed_test_dart/classes/classes.dart';
 import 'package:speed_test_dart/speed_test_dart.dart';
@@ -25,31 +26,32 @@ class _MyAppState extends State<MyApp> {
   bool loadingUpload = false;
 
   Future<void> setBestServers() async {
-      final settings = await tester.getSettings();
-      final servers = settings.servers;
+    final settings = await tester.getSettings();
+    final servers = settings.servers;
 
-      final _bestServersList = await tester.getBestServers(
-        servers: servers,
-      );
+    final _bestServersList = await tester.getBestServers(
+      servers: servers,
+    );
 
-      setState(() {
-        bestServersList = _bestServersList;
-        readyToTest = true;
-      });
-    }
+    setState(() {
+      bestServersList = _bestServersList;
+      readyToTest = true;
+    });
+  }
 
   Future<void> _testDownloadSpeed() async {
-      setState(() {
-        loadingDownload = true;
-      });
-      final _downloadRate =
-          await tester.testDownloadSpeed(servers: bestServersList);
-      setState(() {
-        downloadRate = _downloadRate;
-        _speedValue = downloadRate;
-        loadingDownload = false;
-      });
-    } 
+    setState(() {
+      loadingDownload = true;
+    });
+    final _downloadRate =
+        await tester.testDownloadSpeed(servers: bestServersList);
+    setState(() {
+      downloadRate = _downloadRate;
+      _speedValue = downloadRate;
+      loadingDownload = false;
+    });
+  }
+
   Future<void> _testUploadSpeed() async {
     setState(() {
       loadingUpload = true;
@@ -62,7 +64,15 @@ class _MyAppState extends State<MyApp> {
       _speedValue = uploadRate;
       loadingUpload = false;
     });
-  } 
+  }
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setBestServers();
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -74,7 +84,103 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Gfg Internet Speed Tester'),
         ),
         body: SingleChildScrollView(
-          child: Center(child: Text("Basic UI")),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SfRadialGauge(
+                    enableLoadingAnimation: true,
+                    animationDuration: 4500,
+                    axes: <RadialAxis>[
+                      RadialAxis(minimum: 0, maximum: 60, ranges: <GaugeRange>[
+                        GaugeRange(
+                            startValue: 0, endValue: 20, color: Colors.green),
+                        GaugeRange(
+                            startValue: 20, endValue: 40, color: Colors.orange),
+                        GaugeRange(
+                            startValue: 40, endValue: 60, color: Colors.red)
+                      ], pointers: <GaugePointer>[
+                        NeedlePointer(value: _speedValue)
+                      ], annotations: <GaugeAnnotation>[
+                        GaugeAnnotation(
+                            widget: Container(
+                                child: Text(
+                                    "${_speedValue.toStringAsFixed(2)} Mb/s",
+                                    style: TextStyle(
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.bold))),
+                            angle: 90,
+                            positionFactor: 0.6)
+                      ])
+                    ]),
+                const Text(
+                  'Test Upload speed:',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                if (loadingUpload)
+                  const Column(
+                    children: [
+                      CircularProgressIndicator(),
+                      SizedBox(height: 10),
+                      Text('Testing upload speed...'),
+                    ],
+                  )
+                else
+                  Text('Upload rate ${uploadRate.toStringAsFixed(2)} Mb/s'),
+                const SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                  onPressed: loadingUpload
+                      ? null
+                      : () async {
+                          if (!readyToTest || bestServersList.isEmpty) return;
+                          await _testUploadSpeed();
+                        },
+                  child: const Text('Start'),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                const Text(
+                  'Test Download Speed:',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                if (loadingDownload)
+                  const Column(
+                    children: [
+                      CircularProgressIndicator(),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text('Testing download speed...'),
+                    ],
+                  )
+                else
+                  Text('Download rate ${downloadRate.toStringAsFixed(2)} Mb/s'),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: loadingDownload
+                      ? null
+                      : () async {
+                          if (!readyToTest || bestServersList.isEmpty) return;
+                          await _testDownloadSpeed();
+                        },
+                  child: const Text('Start'),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
